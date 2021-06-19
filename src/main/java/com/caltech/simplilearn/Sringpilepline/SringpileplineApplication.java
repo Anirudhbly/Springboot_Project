951d2b03eb3a24d63459f4801e1e4457abda51d8
student <anirudhbly@gmail.com>
2021-06-19 16:57:52 +0000

    spring boot

---
+++ src/main/java/com/caltech/simplilearn/Sringpilepline/SringpileplineApplication.java
@@ -0,0 +1,13 @@
+package com.caltech.simplilearn.Sringpilepline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SringpileplineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SringpileplineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caltech/simplilearn/Sringpilepline/SringpileplineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caltech.simplilearn.Sringpilepline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SringpileplineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
